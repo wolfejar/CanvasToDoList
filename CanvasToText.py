ca9e8ac1021ccfb2f56640d7e0ca90a2be57aa9f
Jared Wolfe <jaredwolfe@Jareds-MacBook-Pro.local>
2019-01-24 20:33:57 -0600

    Fixed formatting and timezone

--- CanvasToText.py
+++ CanvasToText.py
@@ -1,6 +1,7 @@
 import sys
 import requests
 from dateutil import parser
+import pytz
 
 API_URL = "https://k-state.instructure.com/api/v1/courses"
 
@@ -10,10 +11,11 @@ req_str = requests.get("https://k-state.instructure.com/api/v1/users/self/todo?a
 
 json_obj = req_str.json()
 errors = False
-
 f = open(sys.argv[1], "w")
 
 for assignment in json_obj:
+    due_date = parser.parse(assignment['assignment']['due_at']).astimezone(pytz.timezone("US/Central"))
     f.write(assignment['assignment']['name']
-            + ", " + parser.parse(assignment['assignment']['due_at']).strftime("%a %b %d")
-            + "\n\n")
+            + " due " + due_date.strftime("%A %b %d")
+            + " at " + due_date.strftime("%-I")
+            + ":" + due_date.strftime("%M %p") + "\n\n")
